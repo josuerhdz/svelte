6064736b9a1b7cf98ca108b0f2c45fb5ad67d04d
josue hernandez <josuehernandez1722@gmail.com>
2023-10-17 22:41:50 -0600

    primer commit

---
+++ src/App.svelte
@@ -0,0 +1,8 @@
+<script>
+  import MiComponente from './MiComponente.svelte';
+</script>
+
+<main>
+  <h1>Mi Sitio Web Svelte</h1>
+  <MiComponente />
+</main>
---
+++ src/MiComponente.svelte
@@ -0,0 +1,8 @@
+<!-- src/MiComponente.svelte -->
+<script>
+  let nombre = "Mundo";
+</script>
+
+<main>
+  <h1>Hola, {nombre}!</h1>
+</main>
